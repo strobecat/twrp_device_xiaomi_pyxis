e8c716431117c2953481347056985b89642f2092
strobecat <203321455+strobecat@users.noreply.github.com>
2025-04-04 00:19:44 +0800

    Port to Android 12.1, update README

--- README.md
+++ README.md
@@ -22,24 +22,24 @@ TO-DO:
 
 ## Compile
 
-First checkout minimal twrp with omnirom tree:
+First checkout minimal twrp with AOSP tree:
 
 ```
-repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0
+repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git -b twrp-12.1
 repo sync
 ```
 
 Then add these projects to .repo/manifest.xml:
 
 ```xml
-<project path="device/xiaomi/pyxis" name="mauronofrio/android_device_xiaomi_pyxis" remote="github" revision="android-9.0" />
+<project path="device/xiaomi/pyxis" name="strobecat/twrp_device_xiaomi_pyxis" remote="github" revision="android-12.1" />
 ```
 
 Finally execute these:
 
 ```
 . build/envsetup.sh
-lunch omni_pyxis-eng
+lunch twrp_pyxis-eng
 mka recoveryimage ALLOW_MISSING_DEPENDENCIES=true # Only if you use minimal twrp tree.
 ```
 
--- init/init_pyxis.cpp
+++ init/init_pyxis.cpp
@@ -1,5 +1,5 @@
 /*
-   Copyright (C) 2017-2018 The Android Open Source Project
+   Copyright (C) 2017-2022 The Android Open Source Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -27,51 +27,56 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <cstdlib>
-#include <unistd.h>
-#include <fcntl.h>
-#include <android-base/logging.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <android-base/properties.h>
-
-#include "property_service.h"
-#include "log.h"
+#include <sys/_system_properties.h>
 #include <string>
 #include <fstream>
 
-using namespace std;
+#include "property_service.h"
+#include "vendor_init.h"
 
-namespace android {
-namespace init {
+using android::base::GetProperty;
 
-void load_properties(const char *model) {
-    property_set("ro.product.name", model);
-    property_set("ro.build.product", model);
-    property_set("ro.product.device", model);
+void property_override(char const prop[], char const value[]) {
+  prop_info *pi;
+
+  pi = (prop_info *)__system_property_find(prop);
+  if (pi)
+    __system_property_update(pi, value, strlen(value));
+  else
+    __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 void vendor_load_properties() {
-    property_set("ro.bootimage.build.date.utc", "1546335651");
-    property_set("ro.build.date.utc", "1546335651");
-    const char* path = "/proc/meminfo";
-    std::ifstream infile(path);
-    std::string line;
-    while (std::getline(infile, line))
+  const char* path = "/proc/meminfo";
+  std::ifstream infile(path);
+  std::string line;
+  while (std::getline(infile, line))
+  {
+    if (line.find("MemTotal:") != std::string::npos)
     {
-        if (line.find("MemTotal:") != string::npos)
-        {
-            if (line.substr(17, 7) > "7000000") {
-                load_properties("vela");
-                break;
-            }
-            else
-            {
-                load_properties("pyxis");
-                break;
-            }
-        }
+      if (line.substr(17, 7) > "7000000") {
+        property_override("ro.product.system.device", "vela");
+        property_override("ro.product.system.model", "Xiaomi Mi CC 9 Meitu Edition");
+        property_override("ro.product.vendor.device", "vela");
+        property_override("ro.product.vendor.model", "Xiaomi Mi CC 9 Meitu Edition");
+        property_override("ro.product.odm.device", "vela");
+        property_override("ro.product.odm.model", "Xiaomi Mi CC 9 Meitu Edition");
+        property_override("ro.product.product.device", "vela");
+        property_override("ro.product.product.model", "Xiaomi Mi CC 9 Meitu Edition");
+        property_override("ro.product.system_ext.device", "vela");
+        property_override("ro.product.system_ext.model", "Xiaomi Mi CC 9 Meitu Edition");
+        property_override("ro.product.device", "vela");
+        property_override("ro.product.model", "Xiaomi Mi CC 9 Meitu Edition");
+        property_override("ro.build.product", "vela");
+        property_override("ro.twrp.target.devices", "vela");
+        break;
+      } else {
+        property_override("ro.twrp.target.devices", "pyxis");
+        break;
+      }
     }
-    infile.close();
+  }
+  infile.close();
 }
-
-}  // namespace init
-}  // namespace android
